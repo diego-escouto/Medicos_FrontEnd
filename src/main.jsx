d3c4ae9409cfe635396f63de354cd035c63ef5a1
Diego Escouto <diegonzales72@gmail.com>
2025-12-15 01:58:54 -0300

    resolução de erros

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,23 @@ import { createHashRouter, RouterProvider } from "react-router-dom";
 
 import { AuthProvider } from "./auth/AuthContext";
 
+// Páginas e Layouts
+
 import App from './pages/App.jsx'
 import Sobre from './pages/Sobre.jsx'
 import Contato from './pages/Contato.jsx';
 
-
+// Usuários
 import UsuariosLogin from './pages/usuarios/UsuariosLogin.jsx';
 import UsuariosRegister from './pages/usuarios/UsuariosRegister.jsx';
-import MedicoEdit from './components/medicos/MedicoEdit.jsx';
+
+// Médicos
 import MedicosList from './pages/medicos/MedicosList.jsx';
 import MedicoCreate from './pages/medicos/MedicoCreate.jsx';
+import MedicoEdit from './components/medicos/MedicoEdit.jsx';
+
+// Clínicas
+// import ClinicaEdit from './pages/clinicas/ClinicaEdit.jsx';
 
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.min.js"
@@ -27,6 +34,7 @@ const router = createHashRouter([
     { path: "/medico", element: <MedicosList /> },
     { path: "/medico/:id/edit", element: <MedicoEdit /> },
     { path: "/medico/create", element: <MedicoCreate /> },
+    // { path: "/clinica/:id/edit", element: <ClinicaEdit /> },
 
     { path: "/cliente/login", element: <UsuariosLogin /> },
     { path: "/cliente", element: <UsuariosRegister /> },
